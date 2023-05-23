9439b82a16fca96c3d4022a9006e71d122def802
Diana Tabraue <dianatabraue@gmail.com>
2023-05-23 22:09:43 +0100

    preference mcr

---
+++ api/models/preference.model.js
@@ -0,0 +1,11 @@
+const sequelize = require('../../db/index')
+const { DataTypes } = require('sequelize')
+
+const Preference = sequelize.define('preference', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+})
+
+module.exports = Preference;
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,6 @@ router.use('/profile', require('./profile.route'));
 router.use("/auth", require('./auth.route'));
 router.use('/asadero', require('./asadero.route'))
 router.use('/place', require('./place.route'))
-
+router.use('/preference', require('./preference.route'));
 
 module.exports = router;
