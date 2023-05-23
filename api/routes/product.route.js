11f03b194bc15564073dc38e3de30b31114ccb35
Grancan91 <abl.lopper@gmail.com>
2023-05-23 20:46:43 +0100

    CreateProduct Done

--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
+const { createProduct } = require("../controllers/product.controller")
 
-router.use("/", ()=>{console.log("New Product")})
+router.use("/", createProduct)
 
 module.exports = router
