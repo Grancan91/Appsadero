4f3c0ed9ce0744d750db328d20233cf07a7c0a01
Diana <dianatabraue@gmail.com>
2023-05-24 09:42:06 +0100

    merge diana

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,9 @@ router.use('/profile', require('./profile.route'));
 router.use("/auth", require('./auth.route'));
 router.use('/asadero', require('./asadero.route'))
 router.use('/place', require('./place.route'))
+router.use('/preference', require('./preference.route'));
+router.use('/product', require('./product.route'))
+router.use('/cart', require('./cart.route'))
 
 
 module.exports = router;
