6c9f59e3d839289b1fab5713ed629212fd7651e8
Grancan91 <abl.lopper@gmail.com>
2023-05-24 10:54:31 +0100

    Relation: allergy <=> products

--- db/relationships.js
+++ db/relationships.js
@@ -4,10 +4,10 @@ const User = require('../api/models/user.model')
 const Preference = require('../api/models/preference.model')
 const Product = require('../api/models/product.model')
 const Cart = require('../api/models/cart.model')
-
+const Allergy = require('../api/models/allergy.model')
 
 const initRelationships = () => {
-    
+
     //@OneToMany Preferences w Products
     /*
     Preference.hasMany(Product)    
@@ -19,13 +19,16 @@ const initRelationships = () => {
     User.belongsToMany(Preference, {through: 'user_preference'})
     Preference.belongsToMany(User, {through: 'user_preference'})
     */
-    
+
+    //Allergies Many to Many with Products
+    Allergy.belongsToMany(Product, { through: allergy_products })
+    Product.belongsToMany(Allergy, { through: allergy_products })
 
 
 
 
 }
 
-module.exports = { 
-    initRelationships 
+module.exports = {
+    initRelationships
 }
